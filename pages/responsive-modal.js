73cb63f5f37e55c7c3a7b27545eae3024887cd52
pk <pk@bitbucket.org>
2021-02-16 23:09:06 -0600

    Change homepage content. Add responsive modal link.

--- pages/responsive-modal.js
+++ pages/responsive-modal.js
@@ -1,7 +1,10 @@
 import React from "react";
 
+//Components
+import Modal from "../components/ResponsiveModal"
+
 const ResponsiveModal = () => {
-  return "test";
+  return <Modal />;
 }
 
 export default ResponsiveModal;
